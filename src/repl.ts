20549089391fb4f51501ae21993e906eba87dd47
Xavier FILIPPI <filippi.xaver@yahoo.fr>
2025-06-20 17:27:19 +0200

    Create erzats of cli

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,28 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout} from "node:process";
+
 export function cleanInput(input: string):string[] {
     return input
         .toLowerCase()
         .trim()
         .split(/\s+/)
         .filter(elem => elem);
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: stdin,
+        output: stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        const clean = cleanInput(input);
+        if (clean.length) {
+            console.log(`Your command was: ${clean[0]}`)
+        }
+        rl.prompt();
+    })
 }
